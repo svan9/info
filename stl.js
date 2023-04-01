b22d8b90425324ffbed75abca71bd16c358afc19
svan9 <75327234+svan9@users.noreply.github.com>
2023-04-01 18:46:20 +0300

    1.0.7

--- stl.js
+++ stl.js
@@ -35,8 +35,7 @@ async function readLocalFile(path) {
 
 [...$("flist")].forEach(async (e) => {
     const fname = [...e.attributes].find((a) => a.name == "name").value;
-    const fval = (await readLocalFile(fname)).trim().split(/\r\n/gm);
-    console.log(fval);
+    const fval = (await readLocalFile(fname)).trim().split(/;/gm);
     let htmlList = "";
     fval.forEach((val) => {
         htmlList += `<label title="${val.split(" ")[0]}"><a href="${val.split(" ")[0]}">${
